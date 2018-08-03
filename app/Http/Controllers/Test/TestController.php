224cf6fd5333cb95278ab801252ca14d19da8a46
dangbing <bing.dang@zebra-c.com>
2018-08-03 10:54:16 +0800

    first commit

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        echo 123;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,4 @@
+<?php
+
+
+Route::get('/test', 'Test\TestController@test');
