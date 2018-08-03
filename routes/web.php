c174ed372e5c7691b6a69cb02806f78a06fba428
dangbing <bing.dang@zebra-c.com>
2018-08-03 16:42:43 +0800

    login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 
@@ -19,12 +33,4 @@ Route::get('/', 'PagesController@root')->name('root');
 
 
 
-
-
-
-
-
-
-
-
-Route::get('/test', 'Test\TestController@test');
+Route::get('/test', 'Test\Test@test');
